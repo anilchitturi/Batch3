0d3544eda1d9627de3efb15d69634b731e733de0
anilchitturi <anilkumarchitturi@gmail.com>
2015-01-02 06:00:58 +0530

    updated

---
+++ src/webdriverExamples/ChromeExample.java
@@ -0,0 +1,26 @@
+package webdriverExamples;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class ChromeExample {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub 
+		System.setProperty("webdriver.chrome.driver", "D:/Chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		//driver.get("http://uname:pwd@www.google.com");
+		Thread.sleep(10000);
+		WebElement element=driver.findElement(By.name("q"));
+		element.sendKeys("books");
+		Thread.sleep(10000);
+		element.submit();	
+		Thread.sleep(10000);
+
+		//driver.quit();	
+	}
+
+}
